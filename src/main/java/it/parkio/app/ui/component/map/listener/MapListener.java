f1a1fe6c05b178e64d20151151e92ad74e370c71
Gurwi <gs3335769@gmail.com>
2026-03-05 19:04:06 +0100

    Add parking lot hover actions and implement click detection for parking lots and spaces on the map

--- src/main/java/it/parkio/app/ui/component/map/listener/MapListener.java
+++ src/main/java/it/parkio/app/ui/component/map/listener/MapListener.java
@@ -1,12 +1,16 @@
 package it.parkio.app.ui.component.map.listener;
 
 import it.parkio.app.manager.ParkingLotsManager;
-import it.parkio.app.ui.ParkIOFrame;
+import it.parkio.app.model.ParkingLot;
+import it.parkio.app.model.ParkingSpace;
 import org.jetbrains.annotations.NotNull;
 import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.viewer.GeoPosition;
 
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.Optional;
 
 public class MapListener extends MouseAdapter {
 
@@ -22,15 +26,34 @@ public class MapListener extends MouseAdapter {
 
     @Override
     public void mousePressed(@NotNull MouseEvent e) {
-        if (e.getButton() == MouseEvent.BUTTON3) {
-            ParkIOFrame.LOGGER.debug("Button 3 pressed, requesting bounds...");
+        GeoPosition position = mapViewer.convertPointToGeoPosition(e.getPoint());
 
-//            drawerMouseAdapter.getInputBounds(mapViewer.convertPointToGeoPosition(e.getPoint())).onInput(optionalBounds -> {
-//                optionalBounds.ifPresentOrElse(
-//                        bounds -> ParkIOFrame.LOGGER.debug("Got bounds: {}", bounds),
-//                        () -> ParkIOFrame.LOGGER.debug("Got no bounds")
-//                );
-//            });
-        }
+        getClickedParkingLot(position).ifPresentOrElse(
+                _ -> mapViewer.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)),
+                () -> mapViewer.setCursor(Cursor.getDefaultCursor())
+        );
     }
+
+    @Override
+    public void mouseMoved(@NotNull MouseEvent e) {
+        GeoPosition position = mapViewer.convertPointToGeoPosition(e.getPoint());
+
+        getClickedParkingLot(position).ifPresentOrElse(
+                _ -> mapViewer.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)),
+                () -> mapViewer.setCursor(Cursor.getDefaultCursor())
+        );
+    }
+
+    private @NotNull Optional<ParkingLot> getClickedParkingLot(@NotNull GeoPosition clickedPosition) {
+        return lotsManager.getParkingLots().stream()
+                .filter(lot -> lot.getBounds().contains(clickedPosition))
+                .findFirst();
+    }
+
+    private @NotNull Optional<ParkingSpace> getClickedParkingSpace(@NotNull ParkingLot parkingLot, @NotNull GeoPosition clickedPosition) {
+        return parkingLot.getSpaces().stream()
+                .filter(lot -> lot.getBounds().contains(clickedPosition))
+                .findFirst();
+    }
+
 }
